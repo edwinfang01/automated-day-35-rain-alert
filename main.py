2e4d3179cd007b915bd4da57315740f6257fe61e
edwinfang01 <edwinfang05@gmail.com>
2026-06-30 00:42:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ OWM_endpoint = 'https://api.openweathermap.org/data/2.5/forecast'
 weather_params = {
     'lat': 18.486057,
     'lon': -69.931213,
-    'appid': OWM_token,
+    'appid': 'test',
     'cnt': 4
 }
 
@@ -31,7 +31,7 @@ print(condition_codes)
 for time_stamp in time_stamps:
     weather_id = time_stamp['weather'][0]['id']
     if weather_id < 700:
-        client = Client(account_sid, auth_token)
+        client = Client(account_sid, OWM_token)
         message = client.messages.create(
         body="It's going to rain today. Remember to bring an umbrella!",
         from_="whatsapp:+14155238886",
