d595b156704b308348fc169712aff36780a84e1d
edwinfang01 <edwinfang05@gmail.com>
2026-06-29 22:00:26 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+import requests
+import os
+from twilio.rest import Client
+from dotenv import load_dotenv
+
+account_sid = os.environ.get("TWILIO_ACCOUNT_SID")
+auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
+
+print(account_sid, auth_token)
+
+
+
+OWM_endpoint = 'https://api.openweathermap.org/data/2.5/forecast'
+weather_params = {
+    'lat': 18.486057,
+    'lon': -69.931213,
+    'appid': '55c2ddebc0ef4ade85021b196e786346',
+    'cnt': 4
+}
+
+response = requests.get(url=OWM_endpoint, params=weather_params)
+response.raise_for_status()
+
+data = response.json()
+time_stamps = data['list']
+
+condition_codes = [time_stamp['weather'][0]['id'] for time_stamp in time_stamps]
+
+print(condition_codes)
+
+for time_stamp in time_stamps:
+    weather_id = time_stamp['weather'][0]['id']
+    if weather_id < 700:
+        client = Client(account_sid, auth_token)
+        message = client.messages.create(
+        body="It's going to rain today. Remember to bring an umbrella!",
+        from_="whatsapp:+14155238886",
+        to="whatsapp:+18098160168",
+        )
+
+        print(message.status)
+
+        break
